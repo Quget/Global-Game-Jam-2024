ed0dae5c8266d5d36b6a8af63effbae32a854053
Juan Pablo <73938967+YTBEAR@users.noreply.github.com>
2024-01-27 13:27:09 +0100

    Items combineren

--- Assets/Scripts/SpawnItem.cs
+++ Assets/Scripts/SpawnItem.cs
@@ -5,33 +5,37 @@ using UnityEngine;
 public class SpawnItem : MonoBehaviour
 {
     public GameObject[] items;
-    float timer;
+    public float conveyorSpeed = 2f;
 
     void Start()
     {
-        timer = 2f;
-    }
-
-    void Update()
-    {
-        timer -= Time.deltaTime;
-
-        if ( timer <= 0 )
+        if (items.Length > 0)
         {
-            SpawnRandomObject();
-            timer = 2f;
+            InvokeRepeating("SpawnRandomObject", 0f, 2f);
         }
     }
 
     void SpawnRandomObject()
     {
         int randomIndex = Random.Range(0, items.Length);
-
         GameObject spawnedObject = Instantiate(items[randomIndex], transform.position, Quaternion.identity);
+
+        Rigidbody2D rb = spawnedObject.GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            rb = spawnedObject.AddComponent<Rigidbody2D>();
+        }
+
+        rb.velocity = new Vector2(-conveyorSpeed, 0f);
 
         Debug.Log("Spawned: " + spawnedObject.name);
     }
 
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0f, 0f);
+    }
 }
 
 
