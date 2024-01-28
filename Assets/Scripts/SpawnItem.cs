a2e802b7baf9aaffab2e3b244b4e4b2dcf96b182
Rose Quget <rose@quget.com>
2024-01-28 15:39:43 +0100

    fix wrong include

--- Assets/Scripts/SpawnItem.cs
+++ Assets/Scripts/SpawnItem.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Progress;
-
 public class SpawnItem : MonoBehaviour
 {
     private GameValueService gameValueService;
